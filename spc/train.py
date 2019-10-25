a6a3b321c8392a7a5a1914d9eac0138fc460e43c
bradyz <brady.zhou@utexas.edu>
2019-10-25 03:08:43 -0500

    video grid

--- spc/train.py
+++ spc/train.py
@@ -12,14 +12,14 @@ from .replay_buffer import ReplayBuffer
 from . import controller, policy
 
 
-N_WORKERS = 1
+N_WORKERS = 4
 
 
 class RaySampler(object):
     def __init__(self):
         self.rollouts = [RayRollout.remote() for _ in range(N_WORKERS)]
 
-    def get_samples(self, agent, max_step=1000):
+    def get_samples(self, agent, max_step=2000):
         tick = time.time()
 
         [ray.get(rollout.start.remote()) for rollout in self.rollouts]
@@ -40,16 +40,37 @@ class RaySampler(object):
         print('FPS: %.2f' % (sum(map(len, ros)) / clock))
         print('AFPS: %.2f' % (np.mean(list(map(len, ros))) / clock))
 
+        wandb.log({
+            'fps': (sum(map(len, ros)) / clock),
+            'afps': (np.mean(list(map(len, ros))) / clock),
+            },
+            step=wandb.run.summary['step'])
+
         return ros
 
 
-def log_video(rollout):
-    video = list()
+def log_video(rollouts):
+    videos = list()
 
-    for data in rollout:
-        video.append(data.s.transpose(2, 0, 1))
+    for rollout in rollouts:
+        video = list()
 
-    wandb.log({'video': [wandb.Video(np.uint8(video), format='mp4', fps=20)]})
+        for data in rollout:
+            video.append(data.s.transpose(2, 0, 1))
+
+        videos.append(np.uint8(video))
+
+    t, c, h, w = videos[0].shape
+
+    full = np.zeros((t, c, h * 2, w * 2), dtype=np.uint8)
+    full[:, :, :h, :w] = videos[0]
+    full[:, :, h:, :w] = videos[1]
+    full[:, :, :h, w:] = videos[2]
+    full[:, :, h:, w:] = videos[3]
+
+    wandb.log({
+        'video': [wandb.Video(full, format='mp4', fps=20)]},
+        step=wandb.run.summary['step'])
 
 
 def main(config):
@@ -61,20 +82,22 @@ def main(config):
 
     for epoch in tqdm.tqdm(range(config['max_epoch']+1), desc='epoch', position=0):
         wandb.run.summary['epoch'] = epoch
+        wandb.run.summary['step'] += 1
 
-        for i, rollout in enumerate(RaySampler().get_samples(agent)):
+        rollouts = RaySampler().get_samples(agent)
+
+        for rollout in rollouts:
             for data in rollout:
                 replay.add(data)
 
-            if i == 0:
-                log_video(rollout)
+        log_video(rollouts[:4])
 
     s, a, sp, r, done = replay[[1, 0]]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--max_epoch', type=int, default=3)
+    parser.add_argument('--max_epoch', type=int, default=50)
     parser.add_argument('--batch_size', type=int, default=128)
 
     # Optimizer args.
